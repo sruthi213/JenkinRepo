6fef73fc801a1e8492e206c07d6f96fb134b0062
sruthi213 <srupremsundar@gmail.com>
2020-08-19 17:37:06 +0530

    First commit

---
+++ Myproject/src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+		public static void main(String[] args) {
+			int a=10,b=20,c;
+			c=a+b;
+			System.out.println(c);
+			
+			
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+
+
